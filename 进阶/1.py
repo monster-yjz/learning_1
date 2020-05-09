2934f01e77f4e9bc3d4722c56372ab22f31bf907
monster <1174388274@qq.com>
2020-05-10 01:08:36 +0800

    text

--- 进阶/1.py
+++ 进阶/1.py
@@ -1,8 +1,31 @@
-import re
-from collections import Counter
+# -*- coding:utf-8 -*-
+from random import randint
+from collections import deque
 
-word = open('word.txt', 'r', encoding='UTF-8')
-words = word.read()
-word_list = re.split(' ', words)
-word_most = Counter(word_list).most_common(2)
-print(word_most)
+
+def main():
+    i = randint(1, 10)
+    hq = deque([], 5)
+    times = 0
+    P = True
+    while P:
+        j = input('请输入一个数字')
+        if j.isdigit():
+            n = int(j)
+            hq.append(j)
+            times += 1
+            if int(n) > i:
+                print("猜大了")
+            elif int(n) < i:
+                print("猜小了")
+            elif int(n) == i:
+                print("猜对了！")
+                P = False
+                print("一共猜了" + str(times) + '次')
+            print('历史记录：' + '.'.join(hq))
+        else:
+            print('输入错误')
+
+
+if __name__ == '__main__':
+    main()
